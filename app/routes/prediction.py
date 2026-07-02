d9f878132e34eaef586848b9f884cbe515183a56
Danish faridi <danishfaridi20@gmail.com>
2026-07-02 04:36:54 -0700

    Remove comments from predict function

--- app/routes/prediction.py
+++ app/routes/prediction.py
@@ -7,23 +7,23 @@ prediction_bp = Blueprint("prediction", __name__)
 @prediction_bp.route("/predict", methods=["POST"])
 def predict():
 
-    # Get JSON data from Postman
+
     data = request.get_json()
     if not data:
         return jsonify({"error": "Invalid JSON"})
 
-    # Get disease from JSON
+ 
     disease = data.get("disease")
 
-    # Check if disease are provided
+ 
     if not disease:
         return jsonify({"error": "Please enter disease"})
 
-    # Send disease to AI
+ 
     response = get_ai_response(disease)
 
-    # Convert AI JSON string into Python dictionary
+ 
     response = json.loads(response)
 
-    # Return JSON response
+ 
     return jsonify(response)
